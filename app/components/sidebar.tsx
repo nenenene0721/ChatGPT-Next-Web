964eea77970d9dc6187ff22e0788dda3cf69ca68
nenenene0721 <111256201+nenenene0721@users.noreply.github.com>
2024-02-28 18:35:45 -0500

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          NeneAI
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          ChatGPT.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
