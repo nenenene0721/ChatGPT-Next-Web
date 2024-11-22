b67e0db14c38ae87b6c0ce4539cd5abdbb5da4e6
nenenene0721 <111256201+nenenene0721@users.noreply.github.com>
2024-11-22 18:12:40 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "NeneChat",
+  description: "Chatgpt.",
   appleWebApp: {
-    title: "NextChat",
+    title: "NeneChat",
     statusBarStyle: "default",
   },
 };
