27470d1ee2511058a6410da7b4ccc66bed41cb3e
nenenene0721 <111256201+nenenene0721@users.noreply.github.com>
2025-02-01 13:01:49 +0800

    Update openai.ts

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -195,7 +195,7 @@ export class ChatGPTApi implements LLMApi {
     let requestPayload: RequestPayload | DalleRequestPayload;
 
     const isDalle3 = _isDalle3(options.config.model);
-    const isO1 = options.config.model.startsWith("o1") || options.config.model.startsWith("o3");
+    const isO = options.config.model.startsWith("o");
     if (isDalle3) {
       const prompt = getMessageTextContent(
         options.messages.slice(-1)?.pop() as any,
